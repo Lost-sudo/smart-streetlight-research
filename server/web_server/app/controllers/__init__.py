c973999c59fc81347c1bc0965151842f31162350
John Patrick Paraon <johnpatrickparaon.dev@gmail.com>
2026-04-03 19:11:16 +0800

    feat: export controller classes in controllers package init file

--- server/web_server/app/controllers/__init__.py
+++ server/web_server/app/controllers/__init__.py
@@ -4,4 +4,6 @@ from .streetlight import StreetlightController
 from .user import UserController
 from .streetlight_log import StreetlightLogController
 from .maintenance_log import MaintenanceLogController
-from .predictive_maintenance_log import PredictiveMaintenanceController
+from .predictive_maintenance_log import PredictiveMaintenanceController
+from .alert import AlertController
+
